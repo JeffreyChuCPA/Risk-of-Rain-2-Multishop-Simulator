380a8c6cfdc540e4b5a2532a83498a16d5c7561f
Jeffrey Chu <jeffcolinchu@gmail.com>
2024-02-25 22:41:33 -0500

    worked on CSS of the item selection

--- src/components/MultiShopSelection.tsx
+++ src/components/MultiShopSelection.tsx
@@ -1,57 +1,77 @@
-import { useEffect, useState } from "react"
-import {AllItems} from "./ItemSelection"
-import { Items, UserSelection, itemRarities } from "../utilities/types"
+import { useEffect, useState } from "react";
+import { AllItems } from "./ItemSelection";
+import { Items, UserSelection, itemRarities } from "../utilities/types";
 import "../styles/items.css";
 
-const MultiShopSelection: React.FC<{allItems: AllItems, handleItemSelection: (item: Items) => void , userSelection: UserSelection}> = ({allItems, handleItemSelection, userSelection}) => {
-
-  const [multiShop, setMultiShop] = useState<Items[]>([])
+const MultiShopSelection: React.FC<{
+  allItems: AllItems;
+  handleItemSelection: (item: Items) => void;
+  userSelection: UserSelection;
+}> = ({ allItems, handleItemSelection, userSelection }) => {
+  const [multiShop, setMultiShop] = useState<Items[]>([]);
 
   useEffect(() => {
     const getRandomRarity = (): itemRarities => {
-      const randomNumber: number = Math.floor(Math.random() * 100)
+      const randomNumber: number = Math.floor(Math.random() * 100);
       if (randomNumber < 70) {
-        return itemRarities.common
+        return itemRarities.common;
       } else if (randomNumber < 95) {
-        return itemRarities.uncommon
+        return itemRarities.uncommon;
       } else {
-        return itemRarities.legendary
+        return itemRarities.legendary;
       }
-    }
-  
+    };
+
     const getRandomItemIndex = (itemRarityArray: Items[]): number => {
-      return Math.floor(Math.random() * itemRarityArray.length)
-    }
-  
-    const populateMultiShop = (allItems: AllItems):Items[] => {
-      const multiShopRarity = getRandomRarity()
-      const rolledItems: Items[] = []
-  
+      return Math.floor(Math.random() * itemRarityArray.length);
+    };
+
+    const populateMultiShop = (allItems: AllItems): Items[] => {
+      const multiShopRarity = getRandomRarity();
+      const rolledItems: Items[] = [];
+
       for (let i = 0; i < 3; i++) {
-        const randomItemIndex = getRandomItemIndex(allItems[multiShopRarity].items)
-        rolledItems.push(allItems[multiShopRarity].items[randomItemIndex])
+        const randomItemIndex = getRandomItemIndex(
+          allItems[multiShopRarity].items
+        );
+        rolledItems.push(allItems[multiShopRarity].items[randomItemIndex]);
       }
       return rolledItems;
-      
-    }
+    };
 
-    userSelection.userItems.length < 15 ? setMultiShop(populateMultiShop(allItems)) : setMultiShop([])
-  },[allItems, userSelection])
+    userSelection.userItems.length < 15
+      ? setMultiShop(populateMultiShop(allItems))
+      : setMultiShop([]);
+  }, [allItems, userSelection]);
 
   useEffect(() => {
-    console.log(multiShop)
-  }, [multiShop])
-  
-
-
-
+    console.log(multiShop);
+  }, [multiShop]);
 
   return (
-    <div>
-      {multiShop.length > 0 ? (<div className="multishop-container">{multiShop.map((item, index) => <div className="multishop"> <div className="multishop-top"></div><div className="multishop-cap"></div><img className="item-image" key={`${item.id}+${index}`} src={`public/assets/${item.rarity}/${item.itemName}.webp`} alt={item.itemName} onClick={() => handleItemSelection(item)}/><div className="multishop-bottom"></div></div>)}</div>) : null}
+    <div className="container">
+      {multiShop.length > 0 ? (
+        <div className="multishop-container">
+          {multiShop.map((item, index) => (
+            <div className="multishop">
+              {" "}
+              <div className="multishop-top"></div>
+              <div className="multishop-cap"></div>
+              <img
+                className="item-image"
+                key={`${item.id}+${index}`}
+                src={`public/assets/${item.rarity}/${item.itemName}.webp`}
+                alt={item.itemName}
+                onClick={() => handleItemSelection(item)}
+              />
+              <div className="multishop-bottom"></div>
+              <div className="multishop-bar"></div>
+            </div>
+          ))}
+        </div>
+      ) : null}
     </div>
-    
-  )
-}
+  );
+};
 
-export default MultiShopSelection
+export default MultiShopSelection;
