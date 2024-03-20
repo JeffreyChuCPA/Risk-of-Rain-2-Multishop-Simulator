d8c69a50fe5599de2e60c0d47fae4f4f326155fe
Jeffrey <jeffcolinchu@gmail.com>
2024-03-20 18:31:37 -0400

    updated BE for CORs

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ dotenv.config()
 
 const corsOptions = {
   origin: process.env.FE_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
